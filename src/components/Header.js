d81a334d6dce43b80c4f6a20772431aef95f28c3
Shawn Sutherland <shawnsutherland@hotmail.com>
2023-10-13 15:25:26 +0200

    First commit.

---
+++ src/components/Header.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import Stats from './Stats.js';
+import Stopwatch from './Stopwatch.js';
+
+const Header = (props) => {
+  return (
+    <header>
+      <Stats players={props.players}/>
+      <h1>{props.title}</h1>
+      <Stopwatch />
+    </header>
+  );
+}
+
+export default Header;
